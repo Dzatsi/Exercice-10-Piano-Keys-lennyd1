03ec761bbff0dbce7fe090ba8abeab036a921242
lenny dzatsi <lennydzatsi8@gmail.com>
2023-08-08 10:13:03 +0200

    good function for piano

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
+// const { log } = require("console");
+
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
 keys.forEach(function(key){
   notes.push(document.getElementById(key));
 })
+console.log(keys);
 
 // Write named functions that change the color of the keys below
 function keyPlay(event){
-  const key = event.target;
-  key.style.backgroundColor = "red";
+  event.target.style.backgroundColor = 'yellow';
 };
 function keyReturn(event){
-  const targetElement = event.target;
-  targetElement.style.backgroundColor = '';
+  event.target.style.backgroundColor = '';
 };
 
 // Write a named function with event handler properties
-function assignEvents(note){
-  note.addEventlistener(mousedown, keyPlay);
-  note.addEventlistener(mouseup, keyPlay);
+function assignEvents(note) {
+  note.addEventListener('mousedown', keyPlay);
+  note.addEventListener('mouseup', keyReturn);
 };
 
 // Write a loop that runs the array elements through the function
